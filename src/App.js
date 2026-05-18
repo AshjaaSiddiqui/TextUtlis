dbc280de0b20d8b022432f33ddb230b1f1fabb76
Ashjaa <ashjaa.siddiqui@gmail.com>
2026-05-18 14:49:54 +0500

    tutorial 12

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 
+import { useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
 
 
+
 function App() {
 
-   
+  const [mode, setMode] = useState("light")
+  const handleMode = ()=>
+  {
+    if (mode === "dark")
+    {
+      setMode("light") 
+      document.body.style.backgroundColor = "white"
+    }
+    else 
+    {
+     setMode("dark")
+       document.body.style.backgroundColor = "grey"
+    }
+  } 
+
+
   return (
    <> 
    
   
-  <Navbar  title = "TextUtils" about = "About" />
+  <Navbar  title = "TextUtils" about = "About" mode = {mode} toggleMode = {handleMode} />
    <div className='container my-3'>   
-   <Textform heading = "Enter The Text To Analyze"   />
-      
-        
+   <Textform heading = "Enter The Text To Analyze" mode = {mode}  />
    </div>
 
    </>
--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -31,19 +31,20 @@ export default function Textform(props) {
     return (
     <> 
 
-    <div className='container'>
+    <div className='container' className={`text-${props.mode === 'light' ? 'dark' : 'light'}`} >
        <h1> {props.heading} </h1>
 
        <div className="mb-3">
-       <textarea className="form-control" id="myBox" rows="13" onChange={handleOnChange} value = {Text} ></textarea>
+       <textarea className="form-control" id="myBox" rows="13"   onChange={handleOnChange} value = {Text} 
+        style={{backgroundColor:props.mode === 'light' ? 'white' : 'black' , color: props.mode === 'light' ? 'black' : 'white' }}  ></textarea>
        </div >
 
        <button  className = "btn btn-primary" onClick = {handleUpClick}>Convert To Uppercase</button>
        <button     className = "btn btn-primary mx-1" onClick = {handleLowClick}>Convert To Lowercase</button>
        <button     className = "btn btn-danger mx-1" onClick = {handleClearText}>Clear Text</button>
     </div>
-    <div className='container my-3'>
-      <h2>Your Text Summary</h2>
+    <div className='container my-3'  className={`text-${props.mode === 'light' ? 'dark' : 'light'}`} >
+      <h2 >Your Text Summary</h2>
       <p>   { Text.length == 0 && Text.split(" ").length == 1 ?  "0" : Text.split(" ").length    } Words and {Text.length} Characters </p>
       <p> {0.008 * Text.split(" ").length} Minutes Read </p>
       <h2>Preview</h2>
